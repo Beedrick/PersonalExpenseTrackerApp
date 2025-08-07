85008ba9474091116695d1128f05b0147116630c
Beedrick <pettric.m@yahoo.com>
2025-08-07 17:06:24 +1200

    initial env setup done

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(
+    process.env.MONGO_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then(() => console.log('DB Connection to TransactionWebApp Successful...'))
+.catch((err) => console.error('MongoDB Connection error', err));
+
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+    response = res.json({ status: 'Server is running' });
+    if(response != null){
+        console.log(response);
+    }else{
+        console.log('failed');
+    }
+
+});
+  
+const PORT = 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
